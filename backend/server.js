4483b9dfc1d942971c353870aaa2cdb7a5a41a67
julleb <you@example.com>
2022-01-03 21:22:53 +0100

    cachning av audio

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ var app = express();
 var fs = require("fs");
 const youtube_streamer = require('youtube-audio-stream')
 
-const url = 'http://youtube.com/watch?v=34aQNMvGEZQ'
-
 app.get('/', function (req, res) {
    res.status(200).send("Hello!");
 })
@@ -13,16 +11,16 @@ app.get('/', function (req, res) {
 app.get('/audio', function (req, res) {
     youtubeUrl = req.query.url
     if(!youtubeUrl) {
-        youtubeUrl = url;
+        res.status(201).send("No url");
     }
     console.log(youtubeUrl);
     const url = new URL(youtubeUrl);
-    console.log(url.hostname);
     if(url.hostname !== "www.youtube.com") {
         res.status(500).send("Bad url");
     }
     try {
         youtube_streamer(youtubeUrl).pipe(res);
+        res.set('Cache-Control', 'public, max-age=31557600');
     }catch(err) {
         res.status(500).send("Failed to stream. " + err);
     }
