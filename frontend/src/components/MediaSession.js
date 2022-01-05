8083e8e869f9c8df3f2ee2a98b16aea8128c397a
julleb <you@example.com>
2022-01-05 16:52:30 +0100

    Glömde mediasession

---
+++ frontend/src/components/MediaSession.js
@@ -0,0 +1,37 @@
+import React, { useEffect }  from 'react';
+
+export function MediaSession({track, nextTrackFunction, previousTrackFunction}) {
+  
+    useEffect(() => {
+        if ('mediaSession' in navigator && track) {
+            navigator.mediaSession.metadata = new window.MediaMetadata({
+                title: track.title,
+                artwork: [
+                  { src: track.thumbNail, sizes: '96x96', type: 'image/png' },
+                  { src: track.thumbNail, sizes: '128x128', type: 'image/png' },
+                  // More sizes, like 192x192, 256x256, 384x384, and 512x512
+                ]
+              });
+        }
+
+      }, [track]);
+
+
+      useEffect(() => {
+        if ('mediaSession' in navigator && nextTrackFunction) {
+            navigator.mediaSession.setActionHandler('nexttrack', nextTrackFunction);
+        }
+      }, [nextTrackFunction]);
+
+      useEffect(() => {
+        if ('mediaSession' in navigator && previousTrackFunction) {
+            navigator.mediaSession.setActionHandler('previoustrack', previousTrackFunction);
+        }
+      }, [previousTrackFunction]);
+
+      return (
+        <div>
+            
+        </div>
+      );
+    }
