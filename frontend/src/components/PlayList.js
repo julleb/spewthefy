47cfa2e8e2b5415748c1ee70aeb66b396f84a42e
julleb <you@example.com>
2022-01-06 00:07:15 +0100

    spellista css fixad

--- frontend/src/components/PlayList.js
+++ frontend/src/components/PlayList.js
@@ -1,28 +1,28 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AudioPlayer } from './AudioPlayer';
 import { YoutubeSearcher } from './YoutubeSearcher';
 import { MediaSession } from './MediaSession';
 import ServerApi from './ServerApi'
 
-export function PlayList({playListName}) {
-    
+export function PlayList({ playListName }) {
+
     const [playList, setPlayList] = useState();
     const [currentTrack, setCurrentTrack] = useState()
 
-    const [debugText, setDebugText] = useState()    
+    const [debugText, setDebugText] = useState()
 
-    useEffect (() => {
-            //init playList
-            getPlayList();
+    useEffect(() => {
+        //init playList
+        getPlayList();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
+    }, []);
 
     function nextTrack() {
-        if(!currentTrack) return;
-        for (var i=0; i < playList.length; i++) {
-            if(playList[i] === currentTrack) {
-                var nextSongIndex = i+1;
-                if(nextSongIndex === playList.length) {
+        if (!currentTrack) return;
+        for (var i = 0; i < playList.length; i++) {
+            if (playList[i] === currentTrack) {
+                var nextSongIndex = i + 1;
+                if (nextSongIndex === playList.length) {
                     nextSongIndex = 0;
                 }
                 setCurrentTrack(playList[nextSongIndex]);
@@ -30,14 +30,14 @@ export function PlayList({playListName}) {
         }
     }
     function previousTrack() {
-        if(!currentTrack) return;
-        for (var i=0; i < playList.length; i++) {
-            if(playList[i] === currentTrack) {
-                var trackIndex = i-1;
-                if(trackIndex < 0) {
+        if (!currentTrack) return;
+        for (var i = 0; i < playList.length; i++) {
+            if (playList[i] === currentTrack) {
+                var trackIndex = i - 1;
+                if (trackIndex < 0) {
                     trackIndex = playList.length - 1;
                 }
-                setCurrentTrack(playList[trackIndex]);   
+                setCurrentTrack(playList[trackIndex]);
             }
         }
     }
@@ -54,49 +54,51 @@ export function PlayList({playListName}) {
 
     function removeTrack(track) {
         ServerApi.removeFromPlayList(playListName, track.youtubeUrl)
-        .then(response => {
-            if(response.ok) {
-                setPlayList(playList.filter(x => 
-                    x.youtubeUrl !== track.youtubeUrl
-                ))
-            }else {
-                console.log("Failed to get playlistName " + playListName);
-            }
-        })
+            .then(response => {
+                if (response.ok) {
+                    setPlayList(playList.filter(x =>
+                        x.youtubeUrl !== track.youtubeUrl
+                    ))
+                } else {
+                    console.log("Failed to get playlistName " + playListName);
+                }
+            })
     }
 
     function getPlayList() {
         ServerApi.getPlayList(playListName)
-        .then(response => {
-            if(response.ok) {
-                return response.json();
-            }else {
-                console.log("Failed to get playlistName " + playListName);
-                setPlayList([])
-            }
-        }).then(data => {
-            setPlayList(data);
-        });
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.log("Failed to get playlistName " + playListName);
+                    setPlayList([])
+                }
+            }).then(data => {
+                setPlayList(data);
+            });
     }
 
-    return(
+    return (
         <div className="playlist">
 
-            <YoutubeSearcher playListName={playListName} playList={playList} setPlayList={setPlayList}/>
+            <YoutubeSearcher playListName={playListName} playList={playList} setPlayList={setPlayList} />
             <h2>PlayList: {playListName}</h2>
             <h3>{debugText}</h3>
-            <ol>
-                {playList?.map((track) =>  
-                        <li>
-                            <div>
-                                {track.title}
-                                <button onClick={() => playTrack(track)}>Play</button>
-                                <button onClick={() => removeTrack(track)}>Remove</button>
+
+            <div className="d-flex justify-content-center">
+                <ul className="list-group w-50">
+                    {playList?.map((track) =>
+
+                        <li onClick={() => playTrack(track)} className={`${currentTrack?.youtubeUrl === track.youtubeUrl ? "active" : ""} list-group-item`}>{track.title}
+                            <div className="d-flex justify-content-end">
+                                <button className="btn" onClick={(event) => { event.stopPropagation(); removeTrack(track); }}><i className="bi bi-trash"></i></button>
                             </div>
-                        </li> 
+                        </li>
                     )
-                } 
-            </ol> 
+                    }
+                </ul>
+            </div>
             <AudioPlayer track={currentTrack} />
             <MediaSession track={currentTrack} nextTrackFunction={nextTrack} previousTrackFunction={previousTrack} />
         </div>
